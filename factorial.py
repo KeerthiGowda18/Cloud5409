b9d2261982cb34759f0c2bb76b2f7259fbc6078e
Keerthi Govindaraju <kr583413@dal.ca>
2020-02-26 19:48:06 -0400

    intitial commit

--- factorial.py
+++ factorial.py
@@ -1,4 +1,26 @@
+import math
+import random
+import time 
+
+random_numbers = open('input.txt', 'w')
+
+for i in range(100):
+  number = random.randint(1,100)
+  random_numbers.write(str(number)+ '\n')
+random_numbers.close()
+
 def factorial(n): 
     return 1 if (n==1 or n==0) else n * factorial(n - 1);  
-num =int(input('enter the number'))
-factorial(num)
+PID=1234
+
+with open("input.txt") as f:
+  for x in f:
+    PID+=1
+    start_time=time.time()
+    num = int(x)
+    print('ProcessID:'+PID)
+    print('The value of N:'+str(num))
+    print('The factorial of N:'+str(factorial(num)))
+    print('time taken:'+str(time.time()-start_time)+'seconds')
+    print("\n")
+f.close()
